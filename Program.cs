fcd8522352fca526fba676bef0186b62a62baf4c
JumperBoost <milwenn59@sfr.fr>
2020-10-30 22:21:42 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
